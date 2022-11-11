93409f9e743ee3a94e5c0eb86748e16e556dab40
Wik <zkac176@live.rhul.ac.uk>
2022-11-11 17:28:42 +0000

    Redeveloped StrStack to no longer extend Stack.

--- calculator/src/main/java/uk/ac/rhul/cs2800/StrStack.java
+++ calculator/src/main/java/uk/ac/rhul/cs2800/StrStack.java
@@ -1,5 +1,7 @@
 package uk.ac.rhul.cs2800;
 
+import java.util.EmptyStackException;
+
 /**
  * Creates a new object StrStack that extends the Stack class. Contains a push method that takes a
  * String as a parameter rather than an Entry object.
@@ -7,7 +9,16 @@ package uk.ac.rhul.cs2800;
  * @author ZKAC176
  *
  */
-public class StrStack extends Stack {
+public class StrStack {
+  private Stack strStack;
+  
+  /**
+   * Constructs an empty Stack object.
+   */
+  public StrStack() {
+    strStack = new Stack();
+  }
+
   /**
    * Pushes the specified String object onto the stack.
    *
@@ -15,6 +26,31 @@ public class StrStack extends Stack {
    */
   public void push(String str) {
     Entry entry = new Entry(str);
-    push(entry);
+    strStack.push(entry);
+  }
+
+  /**
+   * Returns the string item on top of the stack.
+   *
+   * @return string object on the top of the stack
+   */
+  public String pop() {
+    if (strStack.size() == 0) {
+      throw new EmptyStackException();
+    }
+    return strStack.pop().toString();
+  }
+
+  /**
+   * Checks if the stack is empty.
+   *
+   * @return the boolean result relative to size of stack
+   */
+  public boolean isEmpty() {
+    if (strStack.size() == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
--- calculator/src/test/java/uk/ac/rhul/cs2800/TestStrStack.java
+++ calculator/src/test/java/uk/ac/rhul/cs2800/TestStrStack.java
@@ -1,7 +1,9 @@
 package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.EmptyStackException;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,27 +22,33 @@ class TestStrStack {
   @Test
   // Test 26
   // Created class strStack and push(String str) that creates a new entry and pushes it onto a
-  // stack.
+  // stack. Created isEmpty().
   void testNewOpStack() {
     strStack.push(string);
-    assertEquals(strStack.size(), 1, "Push an item of type STRING onto the Stack.");
+    assertFalse(strStack.isEmpty(), "Check that an item has been pushed");
   }
 
   @Test
   // Test 27
-  // Checking that StrStack inherits from Stack correctly.
-  // Currently does not pop correctly.
-  void testPopOpStack() {
-    strStack.push(string);
-    assertEquals(strStack.pop().toString(), "Hello", "Pushing 'Hello' should retun 'Hello'.");
-  }
-
-  @Test
-  // Test 28
-  //
+  // Checking pop throws correct exception.
   void testEmptyStrStack() {
     assertThrows(EmptyStackException.class, () -> strStack.pop(),
         "There is nothing to pop from an empty stack.");
   }
 
+  @Test
+  // Test 28
+  // Fixed isEmpty() to return true/false based on size of stack.
+  void testIsEmptyTrue() {
+    assertTrue(strStack.isEmpty(), "No item pushed so should be empty, return True");
+  }
+
+  @Test
+  // Test 29
+  // Changed pop() to no longer return hard-coded value but toString of Stack pop.
+  void testPushPushPop() {
+    strStack.push(string);
+    strStack.push("World");
+    assertEquals(strStack.pop(), "World", "Pushing twice should pop newest entry.");
+  }
 }
