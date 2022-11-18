e148dbbae9881fd79a26783052d84c4b66619436
Wik <zkac176@live.rhul.ac.uk>
2022-11-18 12:54:04 +0000

    Refactored. RevPolishCalc returns any value fed to it or 0 if empty.

--- calculator/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ calculator/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -1,8 +1,16 @@
 package uk.ac.rhul.cs2800;
 
 public class RevPolishCalc {
-  public float evaluate(String string) {
-    return (float) 0;
+  public float evaluate(String what) {
+    if (what == "") {
+      return (float) 0;
+    } else {
+      String[] splitStr = what.split(" ");
+      if (splitStr.length == 1) {
+        return (float) Float.parseFloat(splitStr[0]);
+      }
+      return (float) 5.0;
+    }
   }
-  
+
 }
--- calculator/src/test/java/uk/ac/rhul/cs2800/TestRevPolish.java
+++ calculator/src/test/java/uk/ac/rhul/cs2800/TestRevPolish.java
@@ -2,16 +2,29 @@ package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestRevPolish {
+  RevPolishCalc rp;
+  
+  @BeforeEach
+  void create() {
+    rp = new RevPolishCalc();
+  }
 
   @Test
   // Test 1
   // Created RevPolishCalc class
   void testEvaluate() {
-    RevPolishCalc rp = new RevPolishCalc();
     assertEquals(rp.evaluate("0"), 0.0,
         "0 should return 0.0 when evaluated.");
+  }
+  
+  @Test
+  // Test 2
+  // RevPolishCalc returns arbitrary value dependent on what is fed.
+  void testSimpleEval() {
+    assertEquals(rp.evaluate("2"), 2.0, "2 should evaluate to 2.0");
   }
 }
