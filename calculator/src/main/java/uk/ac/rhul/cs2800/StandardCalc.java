f2397662012a461c95f4fbccd1661911e6ed9e91
Wik <zkac176@live.rhul.ac.uk>
2022-11-24 13:18:16 +0000

    StandardCalc now accounts for no input and single value inputs.
    Splits provided string based on the empty space in between values.

--- calculator/src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
+++ calculator/src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
@@ -4,6 +4,14 @@ public class StandardCalc implements Calculator {
 
   @Override
   public float evaluate(String what) throws InvalidExpression {
+    if (what == "") {
+      return (float) 0;
+    } else {
+      String[] splitStr = what.split(" ");
+      if (splitStr.length == 1) {
+        return (float) Float.parseFloat(splitStr[0]);
+      }
+    } 
     return (float) 0;
   }
 }
--- calculator/src/test/java/uk/ac/rhul/cs2800/TestStandardCalc.java
+++ calculator/src/test/java/uk/ac/rhul/cs2800/TestStandardCalc.java
@@ -1,7 +1,6 @@
 package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,9 +13,22 @@ class TestStandardCalc {
   }
 
   @Test
+  // Test 1
   // Created StandardCalc class and evaluate method.
   void testCreate() throws InvalidExpression {
     assertEquals(sc.evaluate("0"), 0.0, "Evaluating 0 should return 0.0");
+  }
+
+  @Test
+  // Test 2
+  void testCreateSingle() throws InvalidExpression {
+    assertEquals(sc.evaluate("1"), 1.0, "Evaluating 1 should return 1.0");
+  }
+
+  @Test
+  // Test 3
+  void testEmpty() throws InvalidExpression {
+    assertEquals(sc.evaluate(" "), 0.0, "Evaluating nothing should return 0");
   }
 
 }
