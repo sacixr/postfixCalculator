675f511a6e369b17e923f30b5a7aa4c4b40095b9
Wik <zkac176@live.rhul.ac.uk>
2022-12-11 14:55:23 +0000

    Created CodeRefactor branch to refactor code across all classes.
    Added some missing Javadoc and made sure no checkstyle errors were left.

--- calculator/src/main/java/uk/ac/rhul/cs2800/CalcModel.java
+++ calculator/src/main/java/uk/ac/rhul/cs2800/CalcModel.java
@@ -9,6 +9,17 @@ package uk.ac.rhul.cs2800;
  */
 
 public class CalcModel {
+  private RevPolishCalc rp;
+  private StandardCalc sc;
+
+  /**
+   * Constructs an empty CalcModel object.
+   */
+  public CalcModel() {
+    rp = new RevPolishCalc();
+    sc = new StandardCalc();
+  }
+
   /**
    * Evaluates which calculator class to use. Returns float value.
    *
@@ -20,10 +31,8 @@ public class CalcModel {
    */
   public float evaluate(String expr, boolean infix) throws InvalidExpression {
     if (infix == false) {
-      RevPolishCalc rp = new RevPolishCalc();
       return rp.evaluate(expr);
     } else if (infix == true) {
-      StandardCalc sc = new StandardCalc();
       return sc.evaluate(expr);
     }
     return 0;
--- calculator/src/test/java/uk/ac/rhul/cs2800/TestStandardCalc.java
+++ calculator/src/test/java/uk/ac/rhul/cs2800/TestStandardCalc.java
@@ -2,6 +2,7 @@ package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
